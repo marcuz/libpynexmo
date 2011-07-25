a85883878038ea546ce56fb4e9ebc4dc881dc960
Marco Londero <marco.londero@linux.it>
2011-07-25 22:47:33 +0200

    lame fixes

--- nexmomessage.py
+++ nexmomessage.py
@@ -3,9 +3,8 @@ import urllib2
 import urlparse
 import json
 
-SMS_CHARS = 160
-
 class NexmoMessage:
+
     def __init__(self, s, u, p):
         self.apiserver = s
         self.apiuser = u
@@ -23,9 +22,11 @@ class NexmoMessage:
         self.apifrom = f
         self.apito = t
         self.apimsg = m
-        self.request = "%s?username=%s&password=%s&from=%s&to=%s&text=%s" % (self.apiserver, self.apiuser, self.apipass, self.apifrom, self.apito, self.apimsg) 
+        self.request = "%s?username=%s&password=%s&from=%s&to=%s&text=%s" %
+                (self.apiserver, self.apiuser, self.apipass, self.apifrom,
+                self.apito, self.apimsg) 
 
     def sendRequest(self):
-        print json.load(urllib2.urlopen(self.urlFix(self.request)))
+        print(json.load(urllib2.urlopen(self.urlFix(self.request))))
 
 # EOF
