0ea218e7c9790ed57c934b85ae9a767ecd784b1a
Marco Londero <marco.londero@linux.it>
2011-08-05 11:31:40 +0200

    first release, only text and binary messages supported basic interaction with APIs

--- nexmomessage.py
+++ nexmomessage.py
@@ -5,13 +5,19 @@ import json
 
 class NexmoMessage:
 
-    def __init__(self, s, u, p, f = False, t = False, m = False):
-        self.apiserver = s
-        self.apiuser = u
-        self.apipass = p
-        self.apifrom = f
-        self.apito = t
-        self.apimsg = m
+    def __init__(self, s, u, p, f = False, t = False, m = False, ty = "text",
+            st = False, cl = False, nt = False, vr = False, vl = False,
+            bo = False, ud = False):
+        self.sms = {'server': s, 'username': u, 'password': p, 'sender': f,
+                    'recipient': t, 'message': m, 'type': ty,
+                    'status-report-req': st, 'client-ref': cl,
+                    'network-code': nt, 'vcard': vr, 'vcal': vl, 'body': bo,
+                    'udh': ud }
+        self.smstypes = [
+            'text',
+            'binary',
+            # todo: 'wappush', 'unicode', 'vcal', 'vcard'
+        ]
 
     def urlFix(self, s, charset = 'utf-8'):
         if isinstance(s, unicode):
@@ -21,15 +27,67 @@ class NexmoMessage:
         qs = urllib.quote_plus(qs, ':&=')
         return urlparse.urlunsplit((scheme, netloc, path, qs, anchor))
 
-    def textMessage(self, f, t, m):
-        self.apifrom = f
-        self.apito = t
-        self.apimsg = m
+    def setTextInfo(self, m):
+        # automatically transforms msg to text SMS
+        self.sms['type'] = 'text'
+        self.sms['message'] = m
+
+    def setBinInfo(self, b, u):
+        # automatically transforms msg to binary SMS
+        self.sms['type'] = 'binary'
+        self.sms['body'] = b
+        self.sms['udh'] = u
+
+    def checkSMS(self):
+        """ http://www.nexmo.com/documentation/index.html#request """
+        # mandatory parameters
+        if not self.sms['server'] or not self.sms['username'] or \
+               not self.sms['password'] or not self.sms['sender'] or \
+               not self.sms['recipient']:
+            return False
+        # SMS logic, check Nexmo doc for details
+        if self.sms['type'] not in self.smstypes:
+            return False
+        if self.sms['type'] == 'text' and not self.sms['message']:
+            return False
+        if self.sms['type'] == 'binary' and (not self.sms['body'] or \
+                not self.sms['udh']):
+            return False
+        if self.sms['type'] == 'vcard' and not self.sms['vcard']:
+            return False
+        if self.sms['type'] == 'vcal' and not self.sms['vcal']:
+            return False
+        return True
+
+    def buildRequest(self):
+        # check SMS logic
+        if self.checkSMS():
+            # basic request
+            self.request = "%s?username=%s&password=%s&from=%s&to=%s&type=%s" % \
+                (self.sms['server'], self.sms['username'],
+                 self.sms['password'], self.sms['sender'],
+                 self.sms['recipient'], self.sms['type'])
+            # text message
+            if self.sms['type'] == 'text':
+                self.request += "&text=%s" % self.sms['message']
+            # binary message
+            if self.sms['type'] == 'binary':
+                self.request += "&body=%s&udh=%s" % (self.sms['body'],
+                    self.sms['udh'])
+            return self.request
+        return False
+
+    def getDetails(self):
+        return self.sms
 
     def sendRequest(self):
-        self.request = "%s?username=%s&password=%s&from=%s&to=%s&text=%s" % \
-                (self.apiserver, self.apiuser, self.apipass, self.apifrom,
-                 self.apito, self.apimsg) 
-        print(json.load(urllib2.urlopen(self.urlFix(self.request))))
+        if not self.buildRequest():
+            return False
+        # for the future (XML)
+        if self.sms['server'] == 'http://rest.nexmo.com/sms/json':
+            return self.sendRequestJson(self.request)
+
+    def sendRequestJson(self, r):
+        return json.load(urllib2.urlopen(self.urlFix(r)))
 
 # EOF
--- nexmosample.py
+++ nexmosample.py
@@ -12,13 +12,19 @@ def main():
     f = "marcuz"
     t = "+39**********"
     m = "fool the reader àèìòù !!!"
+    bb = "0011223344556677"
+    bu = "06050415811581"
 
-    sms1 = NexmoMessage(s, u, p)
-    sms1.textMessage(f, t, m)
-    sms1.sendRequest()
+    sms1 = NexmoMessage(s, u, p, f, t, m)
+    print("SMS details: %s") % sms1.getDetails()
+    print sms1.sendRequest()
 
-    sms2 = NexmoMessage(s, u, p, f, t, m)
-    sms2.sendRequest()
+    print
+
+    sms2 = NexmoMessage(s, u, p, f, t)
+    sms2.setBinInfo(bb, bu)
+    print("SMS details: %s") % sms2.getDetails()
+    print sms2.sendRequest()
 
 if __name__ == "__main__":
     sys.exit(main());
